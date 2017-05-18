714557253fa6d45586feaaa27ee5f41f88d83297
mmateja <mmateja34@gmail.com>
2017-05-18 19:26:43 +0200

    Add save to db action

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -3,6 +3,7 @@ var db = null;
 function init() {
 	document.addEventListener('deviceready', function() {
 		db = window.openDatabase("bibliotekarz", "1.0", "bibliotekarz", 1000000);
+		db.transaction(setup, errorHandler, dbReady);
 	});
 
  	var online = window.navigator.onLine;
@@ -13,5 +14,20 @@ function init() {
 
 function setup(tx){
 	tx.executeSql('create table if not exist books(id integer primary key autoincrement, isbn text, title text, createdAt date)');
-	
+}
+
+function errorHandler(e){
+	alert(e.message);
+}
+
+fuction dbReady(){
+	$('#add-book-button').on("touchstart", function(e){
+		db.transaction(function(tx){
+			var isbn = "isbn";
+			var title = "title";
+			var createdAt = new Date();
+			createdAt.setDate(createdAt.getDate() - randRange(1,30));
+			tx.executeSql("insert into books(isbn, title, createdAt) values(?,?,?)",[isbn, title, createdAt.getTime()]);
+		}, errorHandler, function() {alert("Książka została dodana")});
+	});
 }
